0cbfc6066715dcb3d612586ad4b7cf5e914ea8ca
carlos <carloslonghi.cl@gmail.com>
2023-02-09 12:59:39 -0300

    feat: create route params

--- src/server.js
+++ src/server.js
@@ -3,6 +3,10 @@ const express = require('express')
 // starting express
 const app = express()
 
+app.get('/message/:id', (request, response) => {
+  response.send(`Id da mensagem: ${request.params.id}`)
+})
+
 // API Port
 const PORT = 3333
 app.listen(PORT, () => {
