b9b71f74940f423ce0e13176d3eb9877d31f1f96
carlos <carloslonghi.cl@gmail.com>
2023-02-10 16:45:26 -0300

    feat: create insertion in TABLE users

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,18 +1,25 @@
-const AppError = require("../utils/AppError")
+const AppError = require('../utils/AppError')
 
-const sqliteConnection = require("../database/sqlite")
+const sqliteConnection = require('../database/sqlite')
 
 class UsersController {
   async create(request, response) {
     const { name, email, password } = request.body
 
     const database = await sqliteConnection()
-    const checkUserExists = await database
-    .get(`SELECT * FROM users WHERE email = (?)`, [email])
+    const checkUserExists = await database.get(
+      'SELECT * FROM users WHERE email = (?)',
+      [email]
+    )
 
-    if(checkUserExists) {
+    if (checkUserExists) {
       throw new AppError('Esse email já esta em uso!')
     }
+
+    await database.run(
+      'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
+      [name, email, password]
+    )
 
     return response.status(201).json()
   }
